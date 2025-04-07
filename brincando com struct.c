7baf36b8e59a6be418a2b725d0f6a0fd57b98012
Pedro Lucas <pedrolucas.larini@gmail.com>
2025-04-07 20:53:15 -0300

    brincando com struct.c
    aprendendo

---
+++ brincando com struct.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+#include <locale.h>
+
+#define tam 50
+
+
+ struct pessoa{
+    int idade;
+    float altura;
+    char nome [tam];
+};
+
+typedef struct pessoa pessoa;
+
+int main(){
+        setlocale(LC_ALL,"Portuguese");
+        
+        pessoa p = {17,1.70,"Pedro"};
+        
+        
+        printf("\nsua idade :%d\n", p.idade);
+        printf("\nseu nome :%s\n", p.nome);
+        printf("\nsua altura:%.2f\n", p.altura);
+        
+        p.idade=18;
+        p.altura=2.04;
+        strcpy(p.nome,"lucas");
+        
+        printf("\nalterando via codigo\n");
+        printf("\nsua idade :%d\n", p.idade);
+        printf("\nseu nome :%s\n", p.nome);
+        printf("\naltura:%.2f\n", p.altura);
+        
+        printf("\nescreva sua idade:\n");
+        scanf("%d", &p.idade);
+        printf("\nescreva seu nome :\n");
+        scanf("%s", &p.nome);
+        printf("\nescreva sua altura:\n");
+        scanf("%f", &p.altura);
+        
+        printf("\nsua idade:%d\n", p.idade);
+        printf("\nseu nome:%s\n", p.nome);
+        printf("\nsua altura:%f\n", p.altura);
+        
+    
+}
